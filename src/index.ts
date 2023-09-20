47f6f8cd584199402b369df53329b85520883c2c
AA1999 <arshia.aghaei@gmail.com>
2023-09-20 12:19:33 +0330

    First release

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,45 @@
-import { Injector, Logger, webpack } from "replugged";
+import {common, Injector, Logger} from 'replugged';
 
-const inject = new Injector();
-const logger = Logger.plugin("PluginTemplate");
+const injector = new Injector();
+const logger = Logger.plugin("Extra Indent Plugin");
+
+function fixIndent(message: string): string {
+	let lines = message.split('\n');
+	const first_line = lines[0].startsWith('```') ? lines[1] : lines[0];
+
+	// Using a tokenizer because regex is slow.
+	let indent_pattern: Array<string> = [];
+
+	for(let char of first_line) {
+		if(char != ' ' && char != '\t')
+			break;
+		indent_pattern.push(char);
+	}
+	if(indent_pattern.length == 0) // No edits required
+		return message;
+	logger.log(`Found indents: ${indent_pattern}`)
+	let pattern = indent_pattern.join('');
+	for(let index in lines) {
+		lines[index] = lines[index].replace(pattern, '');
+	}
+	return lines.join('\n');
+}
+
+function patchIndent() {
+	injector.before(common.messages, 'sendMessage', (args) => {
+		args[1].content = fixIndent(args[1].content);
+		return args;
+	});
+	injector.before(common.messages, 'editMessage', (args) => {
+		args[2].content = fixIndent(args[2].content);
+		return args;
+	});
+}
 
 export async function start(): Promise<void> {
-  const typingMod = await webpack.waitForModule<{
-    startTyping: (channelId: string) => void;
-  }>(webpack.filters.byProps("startTyping"));
-  const getChannelMod = await webpack.waitForModule<{
-    getChannel: (id: string) => {
-      name: string;
-    };
-  }>(webpack.filters.byProps("getChannel"));
-
-  if (typingMod && getChannelMod) {
-    inject.instead(typingMod, "startTyping", ([channel]) => {
-      const channelObj = getChannelMod.getChannel(channel);
-      logger.log(`Typing prevented! Channel: #${channelObj?.name ?? "unknown"} (${channel}).`);
-    });
-  }
+	patchIndent();
 }
 
 export function stop(): void {
-  inject.uninjectAll();
+  injector.uninjectAll();
 }
