58556416d9de482079e55cb017afdba7b2dbe55c
AA1999 <arshia.aghaei@gmail.com>
2023-09-20 13:46:43 +0330

    Added support for multiple blocks

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,45 @@ import {common, Injector, Logger} from 'replugged';
 
 const injector = new Injector();
 const logger = Logger.plugin("Extra Indent Plugin");
+function find_occurrences(message: Array<string>): Array<number> {
+	let occurrences: Array<number> = [];
+	message.forEach((line, index) => {
+		if(line.startsWith('```'))
+			occurrences.push(index);
+	});
+	return occurrences;
+}
 
 function fixIndent(message: string): string {
 	let lines = message.split('\n');
-	const first_line = lines[0].startsWith('```') ? lines[1] : lines[0];
+	let occurrences = find_occurrences(lines);
 
-	// Using a tokenizer because regex is slow.
-	let indent_pattern: Array<string> = [];
+	logger.log(occurrences);
 
-	for(let char of first_line) {
-		if(char != ' ' && char != '\t')
-			break;
-		indent_pattern.push(char);
-	}
-	if(indent_pattern.length == 0) // No edits required
-		return message;
-	logger.log(`Found indents: ${indent_pattern}`)
-	let pattern = indent_pattern.join('');
-	for(let index in lines) {
-		lines[index] = lines[index].replace(pattern, '');
+	if(occurrences.length % 2 == 1) // Extra ```
+		occurrences.pop();
+
+	for(let i = 0; i < occurrences.length; i += 2) {
+		let startIndex = occurrences[i] + 1;
+		let endIndex = occurrences[i + 1];
+		const first_line = lines[startIndex];
+
+		// Using a tokenizer because regex is slow.
+		let indent_pattern = [];
+
+		for(let char of first_line) {
+			if(char != ' ' && char != '\t')
+				break;
+			indent_pattern.push(char);
+		}
+
+		if(indent_pattern.length == 0)
+			continue; // No edits required for this one.
+		let pattern = indent_pattern.join('');
+		for(let index = startIndex; index < endIndex; ++index) {
+			logger.log(lines[index])
+			lines[index] = lines[index].replace(pattern, '');
+		}
 	}
 	return lines.join('\n');
 }
